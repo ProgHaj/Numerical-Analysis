17fa7e9da3e88a74b9a74dda390c818fa44f815a
Haj <flummeltrollet@gmail.com>
2018-01-23 00:50:59 +0000

    add simple abstraction + test if interpolating poly is equal to 0

--- src/fundamentals.rs
+++ src/fundamentals.rs
@@ -1,3 +1,7 @@
+extern crate num;
+
+use self::num::Num;
+use std::ops::Mul;
 
 /*
  * Evaluates polynomial from nested form using Horner's Method
@@ -6,7 +10,8 @@
  * x-coordinate x at which to evaluate,
  * array of d base points b, if needed (optional)
  */
-pub fn nested_multiplication(d: usize, c: &Vec<f32>, x: f32, b: &Vec<f32>) -> Result<f32, &'static str>
+pub fn nested_multiplication<T> (d: usize, c: &Vec<T>, x: T, b: &Vec<T>) -> Result<T, &'static str>
+    where T: Num + Mul<Output = T> + Copy,
 {
     let mut y = match c.get(d) {
         Some(c_last)    => *c_last,
@@ -15,7 +20,7 @@ pub fn nested_multiplication(d: usize, c: &Vec<f32>, x: f32, b: &Vec<f32>) -> Re
 
 
     for i in (0..d).rev() {
-        y *= (x - b[i]) + c[i];
+        y = y * (x - b[i]) + c[i];
     }
     Ok(y)
 }
@@ -35,6 +40,20 @@ mod tests {
             Ok(result)  =>  result,
             Err(err)    =>  panic!("Error: {}", err),
         };
+        println!("y equals to: {}", y);
+    }
+
+    #[test]
+    fn interpolating_polynomial() {
+        let d: usize  = 3;
+        let c: Vec<f32>  = vec![1., 0.5, 0.5, -0.5];
+        let x: f32 = 1.;
+        let b: Vec<f32> = vec![0., 2., 3.];
+        let y  = match nested_multiplication(d, &c, x, &b) {
+            Ok(result)  =>  result,
+            Err(err)    =>  panic!("Error: {}", err),
+        };
+        assert_eq!(y, 0.);
         println!("y equals to: {}", y);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use fundamentals::nested_multiplication;
 
 fn main () {
     let d: usize  = 3;
-    let c: Vec<f32>  = vec![1., 2., 3., 4.];
-    let x: f32  = 0.5;
-    let b: Vec<f32> = vec![0., 0., 0., 0.];
+    let c: Vec<f32>  = vec![1., 0.5, 0.5, -0.5];
+    let x: f32 = 1.;
+    let b: Vec<f32> = vec![0., 2., 3.];
     let y  = match nested_multiplication(d, &c, x, &b) {
         Ok(result)  =>  result,
         Err(err)    =>  panic!("Error: {}", err),
